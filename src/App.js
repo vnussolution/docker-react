7ceaed5c46760353148b2bd5a325105092813cbb
vnussolution <vnussolution@gmail.com>
2020-12-17 16:39:32 -0800

    change app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 11111111
         </a>
       </header>
     </div>
